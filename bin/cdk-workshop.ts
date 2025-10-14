f29e140317b9cefe86c4af7e8c99d3c1ef87a4ce
Joan Manuel Jaramillo Avila <89425013+LifeRIP@users.noreply.github.com>
2025-10-13 23:58:15 -0500

    fix: comment out unused CdkWorkshopStack instantiation in cdk-workshop.ts

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -4,5 +4,5 @@ import { CdkWorkshopStack } from "../lib/cdk-workshop-stack";
 import { PipelineStack } from "../lib/pipeline-stack";
 
 const app = new cdk.App();
-new CdkWorkshopStack(app, "CdkWorkshopStack");
+//new CdkWorkshopStack(app, "CdkWorkshopStack");
 new PipelineStack(app, "CdkWorkshopPipelineStack");
